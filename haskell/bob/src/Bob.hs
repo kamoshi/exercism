aab9b8699b9f4d8349eca96890c01802ebc769b6
Maciej Jur <maciej@kamoshi.org>
2023-08-15 20:14:47 +0200

    haskell: bob

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,18 @@
 module Bob (responseFor) where
+import Data.Char (isLower, isAsciiUpper, isSpace)
+import Data.List (isSuffixOf)
 
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor s
+  | isQuestion s && isShouting s = "Calm down, I know what I'm doing!"
+  | isQuestion s = "Sure."
+  | isShouting s = "Whoa, chill out!"
+  | isSilence s = "Fine. Be that way!"
+  | otherwise = "Whatever."
+  where
+    chars = filter (not . isSpace)
+    isSilence = null . chars
+    isQuestion = ("?" `isSuffixOf`) . chars
+    isShouting x = all ($ chars x) [not . any isLower, any isAsciiUpper]
+
+
